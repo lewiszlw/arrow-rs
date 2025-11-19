a4b89047532f3cff6e65b708946a65d6de59d7e4
张林伟 <lewiszlw520@gmail.com>
2025-11-19 09:03:58 +0800

    Fix

--- parquet/src/arrow/async_reader/mod.rs
+++ parquet/src/arrow/async_reader/mod.rs
@@ -742,7 +742,7 @@ where
         for leaf_idx in 0..num_leaves {
             if mask.leaf_included(leaf_idx) {
                 let root_idx = schema.get_column_root_idx(leaf_idx);
-                if root_leaf_counts[root_idx] == 1 {
+                if root_leaf_counts[root_idx] == 1 && !root.is_list() {
                     included_leaves.push(leaf_idx);
                 }
             }
